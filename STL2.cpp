6e410657a48ccbb5bf34bb33f0e40cd5aa1b3841
MISHA <vol.mykhailo@gmail.com>
2023-10-13 17:45:04 +0300

    Добавьте файлы проекта.

---
+++ STL2.cpp
@@ -0,0 +1,100 @@
+/*
+#include<iostream>
+#include<vector>
+
+
+
+int main()
+{
+    vector<int> vec{ 1,2,3,4,5 };
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << endl;
+    }
+
+    vector<int>::iterator ptr;
+
+    for (ptr = vec.begin();ptr!=vec.end(); ptr++)
+    {
+        cout << *ptr;
+    }
+}
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <locale> // Для setlocale
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    map<string, string> dictionary;
+    int command;
+
+    cout << "Меню:" << endl;
+    cout << "1 Добавить" << endl;
+    cout << "2 Удалить" << endl;
+    cout << "3 Редактировать" << endl;
+    cout << "4 Поиск" << endl;
+    cout << "5 Выйти" << endl;
+    cout << "Выберите: ";
+    cin >> command;
+
+    switch (command)
+    {
+    case 1:
+        string word, translation;
+        cout << "Введите слово: ";
+        cin >> word;
+        cout << "Введите перевод: ";
+        cin >> translation;
+        dictionary[word] = translation;
+        cout << "Слово добавлено." << endl;
+        break;
+    case 2:
+        string wordToDelete;
+        cout << "Введите слово, которое хотите удалить: ";
+        cin >> wordToDelete;
+        if (dictionary.find(wordToDelete) != dictionary.end()) {
+            dictionary.erase(wordToDelete);
+            cout << "Слово удалено." << endl;
+        }
+        else {
+            cout << "Слово не найдено в словаре." << endl;
+        }
+        break;
+    case 3:
+        string wordToEdit, newTranslation;
+        cout << "Введите слово, которое хотите отредактировать: ";
+        cin >> wordToEdit;
+        if (dictionary.find(wordToEdit) != dictionary.end()) {
+            cout << "Введите новый перевод: ";
+            cin >> newTranslation;
+            dictionary[wordToEdit] = newTranslation;
+            cout << "Слово отредактировано." << endl;
+        }
+        else {
+            cout << "Слово не найдено в словаре." << endl;
+        }
+        break;
+    case 4:
+        string wordToSearch;
+        cout << "Введите слово для поиска: ";
+        cin >> wordToSearch;
+        if (dictionary.find(wordToSearch) != dictionary.end()) {
+            cout << "Перевод: " << dictionary[wordToSearch] << endl;
+        }
+        else {
+            cout << "Слово не найдено в словаре." << endl;
+        }
+        break;
+    case 5:
+        break;
+    default:
+        cout << "Ошибка: Неправильный выбор." << endl;
+    }
+
+    return 0;
+}
